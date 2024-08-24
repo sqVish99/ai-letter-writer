45320d990284f1edc1871b9cc946b0df2945f939
sqVish99 <vmharish@asu.edu>
2024-08-23 18:52:42 -0700

    adding gh pages

--- src/App.test.js
+++ src/App.test.js
@@ -3,4 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  const linkElement = screen.getByText(/Letter Writer/i);
+  expect(linkElement).toBeInTheDocument();
 });
